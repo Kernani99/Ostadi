8c8dd85afb1aefc09943d067d85f1020013f79a6
Karnani - قرناني <halim.karnani3@gmail.com>
2025-10-25 09:45:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3773063615-aada3",
+  "appId": "1:55923403193:web:c330531a98be08250b2e68",
+  "apiKey": "AIzaSyCnG2BDmJJ2FGBSboQ2mCyQMjSpScWNH5M",
+  "authDomain": "studio-3773063615-aada3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "55923403193"
+};
